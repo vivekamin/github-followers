c4933d16aee9f274b77b4f0f6729608ad5526ea9
vivekamin <vivek@yodaplus.com>
2017-11-11 19:34:06 -0800

    minor change

--- src/app/service/github.service.ts
+++ src/app/service/github.service.ts
@@ -15,35 +15,29 @@ export class GithubService{
     }
     searchUser(username){
       this.username = username
-      return this._http.get('https://api.github.com/search/users?q='+this.username+'&sort', this.params())
+      return this._http.get('https://api.github.com/search/users?q='+this.username+'&sort' )
         .map(res => res.json());
     }
 
     getUser(username){
       this.count = 1;
       this.tempu = username;
-        return this._http.get('https://api.github.com/users/'+this.tempu, this.params())
+        return this._http.get('https://api.github.com/users/'+this.tempu )
           .map(res => res.json());
     }
 
     getFollowers(){
-        return this._http.get('https://api.github.com/users/'+this.tempu+"/"+"followers?per_page=50",this.params())
+        return this._http.get('https://api.github.com/users/'+this.tempu+"/"+"followers?per_page=50")
           .map(res => res.json());
     }
 
 
     loadMore(){
       this.count+=1;
-      return this._http.get('https://api.github.com/users/'+this.tempu+"/"+"followers?per_page=50&page="+this.count,this.params())
+      return this._http.get('https://api.github.com/users/'+this.tempu+"/"+"followers?per_page=50&page="+this.count)
         .map(res => res.json());
     }
 
-    private params() {
-        // create authorization header with jwt token
-        let params: URLSearchParams = new URLSearchParams();
-        params.set('client_id', 'd8d1c4270b43ad10e691');
-        params.set('client_secret', 'df227a9d8d339bcfb8a90afad71f9b2b4e59984d');
-            return new RequestOptions({ search: params });
-        }
+
 
 }
